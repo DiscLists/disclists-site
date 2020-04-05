bbdf829b17f07847d708d0133dff017aab718ea3
이강준 <admin@gangjun.dev>
2020-04-05 22:13:05 +0900

    update card

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="bg-gray-900">
+  <div id="app" class="bg-gray-800">
     <NavBar />
     <router-view />
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,23 +3,28 @@
     <div id="header">
       <div class="mt-40">
         <div class="text-5xl">DiscLists</div>
-        <div class="text-xl">(대충 pmh가 좋은소개글 생각해낼꺼임)</div>
+        <div class="text-xl">
+          이것은 다양한 세계에서 소통하기 위해 만들어진 프로젝트입니다.- <br />
+          여러분들이 직접 채널을 창작하고 소통할 수 있으며 또한 홍보도 가능하며
+          <br />
+          모든 것이 자유로운 디스코드입니다. <br />
+        </div>
       </div>
     </div>
     <div id="aboutUs" class="mx-4 lg:mx-64 mt-24">
       <div class="text-center text-4xl">
         about us
       </div>
-      <div>
-        (여기도 아마 pmh 가 적어줄거임.. )
-      </div>
+      <div></div>
     </div>
     <div id="aboutUs" class="mx-4 lg:mx-64 mt-24">
       <div class="text-center text-4xl">
         Service Introduction
       </div>
       <div class="lg:grid grid-cols-3 gap-4 flex justify-center">
-        <ServiceInfoCard />
+        <span v-for="(item, index) in serviceInfo" :key="index">
+          <ServiceInfoCard :data="item" />
+        </span>
       </div>
     </div>
     <div id="aboutUs" class="mx-4 lg:mx-64 mt-24">
@@ -37,6 +42,11 @@
 import ServiceInfoCard from './../components/ServiceInfoCard'
 export default {
   name: 'Home',
+  data: function() {
+    return {
+      serviceInfo: [{ name: 'a', description: 'lorem ipsum asdf', color: '' }]
+    }
+  },
   components: {
     ServiceInfoCard
   }
