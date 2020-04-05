fd73228ca617ab97bf35577d42d264084ab53974
이강준 <admin@gangjun.dev>
2020-04-05 20:48:39 +0900

    yeah

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app" class="bg-gray-900">
+    <NavBar />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import NavBar from "./components/Navbar";
+export default {
+  components: {
+    NavBar
+  }
+};
+</script>
+
+<style>
+#app {
+  height: 100vh;
+  widows: 100vw;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text-center"></div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
